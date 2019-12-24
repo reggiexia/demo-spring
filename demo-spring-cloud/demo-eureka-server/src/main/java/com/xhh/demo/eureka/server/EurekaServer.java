a63471e4d4d992f02be8b71d31c8c928f23fb692
tiger <xiahaihu213@163.com>
2019-12-24 16:37:18 +0800

    add demo-eureka-server

---
+++ demo-spring-cloud/demo-eureka-server/src/main/java/com/xhh/demo/eureka/server/EurekaServer.java
@@ -0,0 +1,20 @@
+package com.xhh.demo.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka server
+ *
+ * @author 扶苏
+ * @version 1.0.0 createTime: 2019-12-24 14:20
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
