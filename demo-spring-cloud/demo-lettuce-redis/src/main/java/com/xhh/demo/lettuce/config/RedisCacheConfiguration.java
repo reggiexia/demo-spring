77978b709ce977142bf2826fb4e8ebb8901b93ac
tiger <xiahaihu213@163.com>
2020-04-02 18:50:44 +0800

    lettuce enableAllAdaptiveRefreshTriggers

--- demo-spring-cloud/demo-lettuce-redis/src/main/java/com/xhh/demo/lettuce/config/RedisCacheConfiguration.java
+++ demo-spring-cloud/demo-lettuce-redis/src/main/java/com/xhh/demo/lettuce/config/RedisCacheConfiguration.java
@@ -1,15 +1,29 @@
 package com.xhh.demo.lettuce.config;
 
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * com.xhh.demo.lettuce.config
@@ -20,12 +34,50 @@ import java.io.Serializable;
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisCacheConfiguration {
 
+    @Autowired
+    private RedisProperties redisProperties;
+
     @Bean
-    public RedisTemplate<String, Serializable> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory){
-        RedisTemplate<String,Serializable> template = new RedisTemplate<>();
+    public RedisTemplate<String, Serializable> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setConnectionFactory(redisConnectionFactory);
         return template;
+    }
+
+    @Bean(destroyMethod = "destroy")
+    public LettuceConnectionFactory lettuceConnectionFactoryUvPv() {
+        List<String> clusterNodes = redisProperties.getCluster().getNodes();
+        Set<RedisNode> nodes = new HashSet<>();
+        clusterNodes.forEach(address -> nodes.add(new RedisNode(address.split(":")[0].trim(), Integer.parseInt(address.split(":")[1]))));
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.setClusterNodes(nodes);
+        clusterConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        clusterConfiguration.setMaxRedirects(redisProperties.getCluster().getMaxRedirects());
+
+        GenericObjectPoolConfig<?> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxIdle(redisProperties.getLettuce().getPool().getMaxIdle());
+        poolConfig.setMinIdle(redisProperties.getLettuce().getPool().getMinIdle());
+        poolConfig.setMaxTotal(redisProperties.getLettuce().getPool().getMaxActive());
+
+        ClusterTopologyRefreshOptions topologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                //开启自适应刷新
+                //.enableAdaptiveRefreshTrigger(ClusterTopologyRefreshOptions.RefreshTrigger.MOVED_REDIRECT, ClusterTopologyRefreshOptions.RefreshTrigger.PERSISTENT_RECONNECTS)
+                //开启所有自适应刷新，MOVED，ASK，PERSISTENT都会触发
+                .enableAllAdaptiveRefreshTriggers()
+                // 自适应刷新超时时间(默认30秒)
+                .adaptiveRefreshTriggersTimeout(Duration.ofSeconds(25)) //默认关闭开启后时间为30秒
+                // 开周期刷新
+                .enablePeriodicRefresh(Duration.ofSeconds(20))  // 默认关闭开启后时间为60秒 ClusterTopologyRefreshOptions.DEFAULT_REFRESH_PERIOD 60  .enablePeriodicRefresh(Duration.ofSeconds(2)) = .enablePeriodicRefresh().refreshPeriod(Duration.ofSeconds(2))
+                .build();
+        LettuceClientConfiguration lettuceClientConfiguration = LettucePoolingClientConfiguration.builder()
+                .poolConfig(poolConfig)
+                .clientOptions(ClusterClientOptions.builder().topologyRefreshOptions(topologyRefreshOptions).build())
+                //将appID传入连接，方便Redis监控中查看
+                //.clientName(appName + "_lettuce")
+                .build();
+
+        return new LettuceConnectionFactory(clusterConfiguration, lettuceClientConfiguration);
     }
 }
